777dd8b31c50c3de9b72b2f4628a08092bf7ebd3
Paweł Hofman <pawel@codetitans.pl>
2021-01-02 22:38:27 +0100

    Added new Traveller API

---
+++ GreatSDK/GreatSDK/Traveller.swift
@@ -0,0 +1,32 @@
+//
+//  Traveller.swift
+//  GreatSDK
+//
+//  Created by Pawel Hofman on 02/01/2021.
+//
+
+import Foundation
+
+public struct Traveller {
+  let id: Int
+  let name: String?
+
+  public init(id: Int, name: String? = nil) {
+    self.id = id
+    self.name = name
+  }
+}
+
+extension Traveller {
+
+  public static func printHello(person: Traveller? = nil) {
+    print(getWelcome(person))
+  }
+
+  private static func getWelcome(_ person: Traveller? = nil) -> String {
+    if let name = person?.name {
+      return "Hello \(name), almighty traveller. Welcome in my castle!"
+    }
+    return "Hello traveller. Welcome here!"
+  }
+}
