ddbbe78c1c64e9135dcb8f0274709ad01aa9b70b
Paweł Hofman <pawel@codetitans.pl>
2021-01-02 23:01:30 +0100

    Made some fields public

--- GreatSDK/GreatSDK/Traveller.swift
+++ GreatSDK/GreatSDK/Traveller.swift
@@ -8,8 +8,8 @@
 import Foundation
 
 public struct Traveller {
-  let id: Int
-  let name: String?
+  public let id: Int
+  public let name: String?
 
   public init(id: Int, name: String? = nil) {
     self.id = id
