cd43dd6846ad2fd091f6561c477ef2ba3461b38a
Paweł Hofman <pawel@codetitans.pl>
2021-01-02 02:15:43 +0100

    Created build script

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+xcodebuild clean archive -workspace GreatSDK/GreatSDK.xcworkspace -scheme "GreatSDK" \
+            -sdk iphoneos OBJROOT=build/iOS SKIP_INSTALL=YES BUILD_LIBRARY_FOR_DISTRIBUTION=YES | xcpretty
+
+xcodebuild clean archive -workspace GreatSDK/GreatSDK.xcworkspace -scheme "GreatSDK" \
+            -sdk iphonesimulator OBJROOT=build/simulator SKIP_INSTALL=YES BUILD_LIBRARY_FOR_DISTRIBUTION=YES | xcpretty
+
+xcodebuild -create-xcframework \
+ -framework GreatSDK/build/iOS/UninstalledProducts/iphoneos/GreatSDK.framework \
+ -framework GreatSDK/build/simulator/UninstalledProducts/iphonesimulator/GreatSDK.framework \
+ -output build/GreatSDK.xcframework
+
